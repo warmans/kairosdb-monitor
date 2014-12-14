55e945a218bb9a06c52bd598aafa81862408cca7
warmans <stefan.warman@switchconcepts.com>
2014-12-14 14:05:26 +0000

    Added alias support to host config

--- config/config.js
+++ config/config.js
@@ -1,9 +1,7 @@
 var config = {}
 
 config.kairos_hosts = [
-    {host: '127.0.0.1:8080'},
-    {host: '127.0.0.1:8080'},
-    {host: '127.0.0.1:8080'}
+    {host: '127.0.0.1:8080', alias: 'localhost'}
 ];
 
 module.exports = config;
